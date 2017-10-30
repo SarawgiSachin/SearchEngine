9a1147527158b343b40911339a2f1d3366ab9bb4
Javabeginner <31772119+LPL2211@users.noreply.github.com>
2017-10-30 15:14:28 +0100

    Update FileHelperTest.java

--- searchengine/test/FileHelperTest.java
+++ searchengine/test/FileHelperTest.java
@@ -1,35 +1,40 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
 
 public class FileHelperTest {
 
 
-    Index SimpleIndex = null;
+    Index InvertedIndex = null;
 
 
     @Before
-    void setUp() {
+    public void setUp() {
         List<Website> sites = new ArrayList<Website>();
-        sites.add(new Website("example1.com", "example1", Arrays.asList("word1", "word2", "word1")));
-        sites.add(new Website("example2.com", "example2", Arrays.asList("word2", "word3")));
-        SimpleIndex = new SimpleIndex();
-        SimpleIndex.build(sites);
+        sites.add(new Website("test1.com", "test1", Arrays.asList("word1", "word2")));
+        sites.add(new Website("test2.com", "test2", Arrays.asList("word2", "word3")));
+        InvertedIndex = new SimpleIndex();
+        InvertedIndex.build(sites);
     }
 
     @After
-    void tearDown() {
-        SimpleIndex = null;
+    public void tearDown() {
+        InvertedIndex = null;
     }
 
     @Test
-    void buildSimpleIndex() {
-        assertEquals("SimpleIndex{sites=[Website{title='example1', url='example1.com', words=[word1, word2, word1]}, Website{title='example2', url='example2.com', words=[word2, word3]}]}", SimpleIndex.toString());
+    public void buildSimpleIndex() {
+        assertEquals("SimpleIndex{sites=[Website{title='example1', url='example1.com', words=[word1, word2, word1]}, Website{title='example2', url='example2.com', words=[word2, word3]}]}", InvertedIndex.toString());
     }
 
     @Test
-    void lookupSimpleIndex() {
-        lookup(SimpleIndex);
+    public void lookupSimpleIndex() {
+        lookup(InvertedIndex);
     }
 
     private void lookup(Index index) {
